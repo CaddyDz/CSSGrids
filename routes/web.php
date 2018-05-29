84ef846af93d9068c1c53f4b2d2bc5ad2a632d95
Salim Djerbouh <github@caddydz.me>
2018-05-29 19:45:05 +0100

    Route groups
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,12 @@
 <?php
 
 Route::get('/', 'HomeController@index');
+
+Route::group(['prefix' => 'account'], function() {
+  Route::get('profile', function() {
+    echo 'Profile';
+  });
+  Route::get('password', function() {
+    echo 'Password';
+  });
+});
