b86e18bb0d9e58acf5cde4b725586ba31a7c4518
Salim Djerbouh <github@caddydz.me>
2018-06-01 14:11:00 +0100

    Basic Controllers
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,12 +8,6 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        echo $request->name . '<br>';
-        echo $request['name'] . '<br>';
-        echo request('name') . '<br>';
-        echo $request->get('name') . '<br>';
-        echo $request->get('name', null) . '<br>';
-        echo $request->get('name', 'no name provided') . '<br>';
-        die();
+        return 'Home';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,6 @@
 <?php
 
-Route::get('/', 'HomeController@index');
-
-Route::get('redirect', function() {
-  return redirect()->route('landing');
-});
-
-Route::get('redirect2', function() {
-  return redirect()->route('landing2');
-});
-
-Route::get('landing/page', function() {
-  echo 'Landing';
-})->name('landing');
-// This also works
-Route::get('landing/page/abc', [
+Route::get('/', [
   'uses' => 'HomeController@index',
-  'as' => 'landing2' // Naming here
+  'as' => 'home'  
 ]);
