dd1e35d767ff23e6f80d041cbc5427ca707f95e2
Salim Djerbouh <github@caddydz.me>
2018-06-01 11:51:41 +0100

    Named Routes
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,19 @@
 
 Route::get('/', 'HomeController@index');
 
-Route::group(['prefix' => 'account'], function() {
-  Route::get('profile', function() {
-    echo 'Profile';
-  });
-  Route::get('password', function() {
-    echo 'Password';
-  });
+Route::get('redirect', function() {
+  return redirect()->route('landing');
 });
+
+Route::get('redirect2', function() {
+  return redirect()->route('landing2');
+});
+
+Route::get('landing/page', function() {
+  echo 'Landing';
+})->name('landing');
+// This also works
+Route::get('landing/page/abc', [
+  'uses' => 'HomeController@index',
+  'as' => 'landing2' // Naming here
+]);
