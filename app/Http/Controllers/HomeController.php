7e722ec714bc8db1d45bbb9933cdec59e3f48e40
Salim Djerbouh <github@caddydz.me>
2018-06-16 19:09:56 +0100

    Setting up to send email
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,14 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-		public function index()
-		{
-			return response()->json([
-				'posts' => [
-					['id' => 1, 'title' => 'abc'],
-					['id' => 2, 'title' => 'def'],
-					['id' => 3, 'title' => 'ghi']
-				]
-			]);
-		}
+    public function index()
+    {
+        return view('index');
+    }
 }
