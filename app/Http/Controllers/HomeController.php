f9a1b44f626da5ab466de398b42d70459f00f5f2
Salim Djerbouh <github@caddydz.me>
2018-06-20 22:15:00 +0100

    Sending email
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,24 @@
 
 namespace Fresh\Http\Controllers;
 
+use Illuminate\Mail\Mailer;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    protected $mailer;
+        
+    public function __construct(Mailer $mailer)
     {
-        return view('index');
+        $this->mailer = $mailer;
+    }
+        
+    public function index(Mailer $mail)
+    {
+        $name = 'Salim';
+                
+        $this->mailer->send('emails.auth.welcome', ['name' => $name], function ($m) {
+            $m->to('salim@caddydz.me', 'Salim')->subject('You have registered.');
+        });
     }
 }
