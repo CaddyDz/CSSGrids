954cb8e4a7122d756591a62ffa36ca9de605a1d9
Salim Djerbouh <github@caddydz.me>
2018-06-21 23:43:24 +0100

    Creating a model
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- app/Post.php
+++ app/Post.php
@@ -8,5 +8,8 @@ class Post extends Model
 {
     protected $table = 'posts';
         
-    protected $fillable = ['title','body'];
+    protected $fillable = [
+            'title',
+            'body'
+        ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
-
 Route::get('/seed', function (\Fresh\Post $post) {
-	$f = \Faker\Factory::create();
+    $f = \Faker\Factory::create();
 
-	forea
+    foreach (range(1, 1000) as $x) {
+        $post->create([
+            'title' => $f->sentence(10),
+            'body' => $f->sentence(100)
+        ]);
+    }
 });
 
 Route::get('/', [
