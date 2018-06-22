df2f5174bb51aca44b7001958504e425d9c832f7
Salim Djerbouh <github@caddydz.me>
2018-06-22 01:03:00 +0100

    Creating, updating and deleting
    Signed-off-by: Salim Djerbouh <github@caddydz.me>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,9 +9,13 @@ class HomeController extends Controller
 {
     public function index(Request $request, Post $post)
     {
-        $postToUpdate = $post->findOrFail($request->id);
-        dd($postToUpdate);
+        $postToUpdate = $post->Where('id', '=', $request->id)->first();
 
+        $postToUpdate->title = 'This is an updated title';
+        $postToUpdate->update();
+        echo $postToUpdate->title;
+        $postToUpdate->delete();
+        dd('Done!');
         return view('index');
     }
 }
